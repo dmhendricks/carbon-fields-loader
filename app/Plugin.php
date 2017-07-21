c74cb2f0f2295fb2eb04a1a58f8244745847693a
Daniel Hendricks <mail@danhendricks.com>
2017-07-21 11:54:04 -0500

    Fixed bug in show_notice()

--- app/Plugin.php
+++ app/Plugin.php
@@ -15,15 +15,16 @@ class Plugin {
 
     // Initialize Carbon Fields and verify dependencies
     add_action( 'plugins_loaded', array( 'Carbon_Fields\\Carbon_Fields', 'boot' ) );
-    add_action( 'carbon_fields_loaded', array( $this, 'verify_dependencies' ) );
+    add_action( 'plugins_loaded', array( &$this, 'verify_dependencies' ) );
 
   }
 
   public function verify_dependencies() {
+
     // Check if outdated version of Carbon Fields loaded
     $error = null;
 
-    if(!defined('\\Carbon_Fields\\VERSION')) {
+    if( !defined('\\Carbon_Fields\\VERSION') ) {
       $error = '<strong>' . self::$settings['data']['Name'] . ':</strong> ' . __('A fatal error occurred while trying to initialize plugin.');
     } else if( version_compare( \Carbon_Fields\VERSION, self::$settings['deps']['carbon_fields'], '<' ) ) {
       $error = '<strong>' . self::$settings['data']['Name'] . ':</strong> ' . __('Unable to load. An outdated version of Carbon Fields has been loaded:' . ' ' . \Carbon_Fields\VERSION) . ' (&gt;= '.self::$settings['deps']['carbon_fields'] . ' ' . __('required') . ')';
@@ -31,16 +32,16 @@ class Plugin {
 
     if($error) $this->show_notice($error, 'error', false);
     return !$error;
+
   }
 
   /**
     * Helper function to display a notice in the WP Admin
     */
   private function show_notice($msg, $type = 'error', $is_dismissible = false) {
-    if(is_admin()) {
-      $class = 'notice notice-'.$type.($is_dismissible ? ' is-dismissible' : '');
-      $msg = __( $msg, self::$settings['data']['TextDomain'] );
 
+    if( is_admin() ) {
+      $class = 'notice notice-' . $type . ( $is_dismissible ? ' is-dismissible' : '' );
       printf( '<div class="%1$s"><p>%2$s</p></div>', $class, $msg );
     }
   }
